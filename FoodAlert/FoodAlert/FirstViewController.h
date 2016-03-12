c66ae264d4462de7f91eba98994b69f4ab5e1d26
Jack Li <drjackl@gmail.com>
2016-03-12 09:06:30 -0800

    Initial project creation

---
+++ FoodAlert/FoodAlert/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  FoodAlert
+//
+//  Created by Jack Li on 3/12/16.
+//  Copyright © 2016 Jack Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
