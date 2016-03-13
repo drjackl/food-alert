7849bd68b584440b4f1da3f96f1c5317da7446eb
Jack Li <drjackl@gmail.com>
2016-03-12 22:53:46 -0800

    Added first tab MapVC

---
+++ FoodAlert/FoodAlert/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  FoodAlert
+//
+//  Created by Jack Li on 3/12/16.
+//  Copyright © 2016 Jack Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
