7fe4d476d1f4330a3dd8fe47e0201bc0d670a9e0
Jack Li <drjackl@gmail.com>
2016-03-16 10:56:11 -0700

    Switch button switches between two views by setting the top view (view2) transparency to 1 or 0

--- FoodAlert/FoodAlert/CoinViewController.m
+++ FoodAlert/FoodAlert/CoinViewController.m
@@ -9,6 +9,8 @@
 #import "CoinViewController.h"
 
 @interface CoinViewController ()
+@property (nonatomic) UIView* currentView;
+// Outlets
 @property (weak, nonatomic) IBOutlet UIView* view1;
 @property (weak, nonatomic) IBOutlet UIView* view2;
 @end
@@ -18,12 +20,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.currentView = self.view1;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction) switchViewControllers {
+    // if currently on view1
+    if (self.currentView == self.view1) {
+        self.view2.alpha = 1.0; // show view2 by making opaque
+        self.currentView = self.view2;
+    } else {
+        self.view2.alpha = 0.0; // show view1 by making view2 clear
+        self.currentView = self.view1;
+    }
+    NSLog(@"Button pressed!");
+}
+
 
 /*
 #pragma mark - Navigation
